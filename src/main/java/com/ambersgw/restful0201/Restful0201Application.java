41cbd9e6c69866dbe1ab5b87aa2fc611b24a62cd
Amber <salalaita@gmail.com>
2022-11-03 23:03:23 +0800

    restful0201再次啟動

---
+++ src/main/java/com/ambersgw/restful0201/Restful0201Application.java
@@ -0,0 +1,13 @@
+package com.ambersgw.restful0201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restful0201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restful0201Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambersgw/restful0201/Restful0201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambersgw.restful0201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restful0201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
